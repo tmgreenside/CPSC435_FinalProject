e144f27417665b7da19ce0b1521ee6ed1bf5f78a
tmgreenside <trevmg7@gmail.com>
2019-04-22 16:12:04 -0700

    Master still needs algorithm

--- master.h
+++ master.h
@@ -3,8 +3,31 @@
 
 #include "myMatrix.h"
 
-void master() {
-    
+int** matrixA, matrixB, matrixC;
+
+void master(int n, int rank, int size) {
+    matrixA = createMatrix(n);
+    matrixB = createMatrix(n);
+    matrixC = createEmptyMatrix(n);
+
+    struct timeval tpstart, tpend;
+    gettimeofday(&tpstart, NULL);
+
+    MPI_Request recv = malloc(size * sizeof(MPI_Request));
+    short firstLoop = 1;
+
+    int i;
+    for (i = 1; i < size; i++) {
+        // TO DO. Send data to Slaves
+    }
+
+    gettimeofday(&tpend, NULL);
+    double timeUse = (tpend.tv_sec - tpstart.tv_sec) + ((double)(tpend.tv_usec - tpstart.tv_usec)/1000000);
+    printf("Time used where n = %d (s): %f\n", n, timeUse);
+
+    freeMatrix(matrixA, n);
+    freeMatrix(matrixB, n);
+    freeMatrix(matrixC, n);
 }
 
 #endif
