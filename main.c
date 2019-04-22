c70dce3771379e0d6ac6c3fc54c7839d47f77220
tmgreenside <trevmg7@gmail.com>
2019-04-22 14:39:07 -0700

    Created repository.

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+void master() {
+
+}
+
+void slave() {
+
+}
+
+int main(int argc, char** argv) {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("Hello from rank %d!\n", rank);
+
+    MPI_Finalize();
+    return 0;
+}
