78a68d5bef6bb4aa8577b9e081438d5aa5c1c6f8
Matthew Lee <kwwxis@users.noreply.github.com>
2019-04-22 16:13:14 -0700

    Add code to main.c

--- main.c
+++ main.c
@@ -8,14 +8,26 @@
 #include "master.h"
 #include "slave.h"
 
+int rank, size, n;
+
 int main(int argc, char** argv) {
-    int rank, size;
+	if (argc != 2) {
+        printf("usage: %s <n>\n", argv[0]);
+        printf("  n: matrix size");
+        return 1;
+    }
+    
+    n = atoi(argv[1]);
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    printf("Hello from rank %d!\n", rank);
+    
+    if (rank == 0) {
+        master();
+    } else {
+        slave();
+    }
 
     MPI_Finalize();
     return 0;
